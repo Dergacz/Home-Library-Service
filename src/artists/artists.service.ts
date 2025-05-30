5aa0f53a1e20769d9820650be5611a03bf61b2a3
Stanislau <sats96dergach@gmail.com>
2025-05-30 17:18:26 +0200

    feat: add Artist

---
+++ src/artists/artists.service.ts
@@ -0,0 +1,81 @@
+import {
+  Injectable,
+  NotFoundException,
+  BadRequestException,
+} from '@nestjs/common';
+import { v4 as uuidv4 } from 'uuid';
+import { Artist } from './entities/artist.entity';
+import { CreateArtistDto } from './dto/create-artist.dto';
+import { UpdateArtistDto } from './dto/update-artist.dto';
+
+@Injectable()
+export class ArtistsService {
+  private artists: Artist[] = [];
+
+  findAll(): Artist[] {
+    return this.artists;
+  }
+
+  findOne(id: string): Artist {
+    if (!this.isValidUUID(id)) {
+      throw new BadRequestException('Invalid artist ID');
+    }
+
+    const artist = this.artists.find((artist) => artist.id === id);
+    if (!artist) {
+      throw new NotFoundException('Artist not found');
+    }
+
+    return artist;
+  }
+
+  create(createArtistDto: CreateArtistDto): Artist {
+    const newArtist: Artist = {
+      id: uuidv4(),
+      name: createArtistDto.name,
+      grammy: createArtistDto.grammy,
+    };
+
+    this.artists.push(newArtist);
+    return newArtist;
+  }
+
+  update(id: string, updateArtistDto: UpdateArtistDto): Artist {
+    if (!this.isValidUUID(id)) {
+      throw new BadRequestException('Invalid artist ID');
+    }
+
+    const artistIndex = this.artists.findIndex((artist) => artist.id === id);
+    if (artistIndex === -1) {
+      throw new NotFoundException('Artist not found');
+    }
+
+    const updatedArtist: Artist = {
+      ...this.artists[artistIndex],
+      name: updateArtistDto.name,
+      grammy: updateArtistDto.grammy,
+    };
+
+    this.artists[artistIndex] = updatedArtist;
+    return updatedArtist;
+  }
+
+  remove(id: string): void {
+    if (!this.isValidUUID(id)) {
+      throw new BadRequestException('Invalid artist ID');
+    }
+
+    const artistIndex = this.artists.findIndex((artist) => artist.id === id);
+    if (artistIndex === -1) {
+      throw new NotFoundException('Artist not found');
+    }
+
+    this.artists.splice(artistIndex, 1);
+  }
+
+  private isValidUUID(id: string): boolean {
+    const uuidRegex =
+      /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+    return uuidRegex.test(id);
+  }
+}
